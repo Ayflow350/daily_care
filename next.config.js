bb0ff04837625fccfe1e891cd9480dec76686817
Ayflow350 <soluwatist@gmail.com>
2024-05-08 09:39:29 +0100

    baseUrl

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = {
+
+
+const nextConfig = {
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
 };
+module.exports = nextConfig;
 
-module.exports = nextConfig

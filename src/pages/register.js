764b5869bca94613a3b239dbcbf95bc7258a8831
Ayflow350 <soluwatist@gmail.com>
2024-05-14 11:37:35 +0100

    apy

--- src/pages/register.js
+++ src/pages/register.js
@@ -9,84 +9,63 @@ import { ClipLoader } from "react-spinners";
 import Layout from "src/layout/Layout";
 import Rating from "src/components/common/Rating";
 import { registerTestimonial, registerTestimonialTarget } from "src/utils/data";
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const baseUrl = `https://daily-care-6y11.vercel.app/`;
 
 const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
-  const router = useRouter();
 
   const handleServerResponse = async (response) => {
     try {
       if (response.ok) {
-        const data = await response.json(); // Parse server response
-        toast.success("Registration successful!"); // Display success message
-        const redirectUrl = data.redirect; // Extract redirect URL
+        const data = await response.json();
+        toast.success("Registration successful!");
+        const redirectUrl = data.redirect;
         if (redirectUrl) {
-          router.push(redirectUrl); // Navigate to the redirect URL
+          router.push(redirectUrl);
         }
-        reset(); // Reset the form after success
+        reset();
       } else {
-        const errorData = await response.json(); // Parse error response
+        const errorData = await response.json();
         console.error("Error processing server response:", errorData.message);
-        toast.error("Server error: " + errorData.message); // Display error message
+        toast.error("Server error: " + errorData.message);
       }
     } catch (error) {
       console.error("Error parsing server response:", error);
-      toast.error("Error parsing server response"); // Display error message
+      toast.error("Error parsing server response");
     }
   };
 
   const onSubmit = async (formData) => {
-    const baseUrl = "https://daily-care-6y11.vercel.app/";
-    setIsLoading(true); // Show loading spinner
+    setIsLoading(true);
     try {
       const response = await fetch(`${baseUrl}/api/user`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
-
-      await handleServerResponse(response); // Handle server response
+      await handleServerResponse(response);
     } catch (error) {
       console.error("Submission error:", error);
-      toast.error("Submission error: " + error.message); // Handle errors
+      toast.error("Submission error: " + error.message);
     } finally {
-      setIsLoading(false); // Hide loading spinner
+      setIsLoading(false);
     }
   };
 
   return (
     <Layout title="Sign Up" desc="This is the sign-up page">
-      <section
-        className="sign-up-in-section bg-dark ptb-60"
-        style={{
-          background: "url('/page-header-bg.svg') no-repeat right bottom",
-        }}
-      >
+      <section className="sign-up-in-section bg-dark ptb-60">
         <div className="container position-relative">
           {isLoading && (
-            <div
-              className="loading-overlay"
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                background: "rgba(0, 0, 0, 0.5)",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                zIndex: 10,
-              }}
-            >
-              <div style={{ marginLeft: "10px", color: "white" }}>
+            <div className="loading-overlay">
+              <div className="loading-content">
                 <ClipLoader size={100} color={"#0000FF"} />
               </div>
             </div>
@@ -94,56 +73,42 @@ const Register = () => {
           <div className="row justify-content-center">
             <div className="col-lg-10 col-12">
               <div className="pricing-content-wrap bg-custom-light rounded-custom shadow-lg">
+                {/* Left Content */}
                 <div className="price-feature-col pricing-feature-info text-white left-radius p-5 order-1 order-lg-0">
-                  <Link href="/">
-                    <a className="mb-5 d-none d-xl-block d-lg-block">
-                      <Image
-                        width={250}
-                        height={100}
-                        src="/vgh.png"
-                        alt="logo"
-                        className="img-fluid"
-                      />
-                    </a>
-                  </Link>
+                  {/* Testimonials */}
                   <div className="customer-testimonial-wrap mt-60">
+                    {/* Testimonials Content */}
                     <div className="tab-content" id="nav-tabContent">
                       {registerTestimonial.map((testimonial, i) => (
                         <div
-                          key={i + 1}
+                          key={i}
                           className={`tab-pane fade ${testimonial.active}`}
                           id={testimonial.target}
                           role="tabpanel"
                         >
+                          {/* Testimonial Content */}
                           <div className="testimonial-tab-content mb-4">
-                            <div className="mb-2">
-                              <ul className="review-rate mb-0 mt-2 list-unstyled list-inline">
-                                <li className="list-inline-item">
-                                  <Rating />
-                                </li>
-                              </ul>
-                            </div>
+                            <Rating />
                             <blockquote>
-                              <h5>{testimonial.header} </h5>
+                              <h5>{testimonial.header}</h5>
                               {testimonial.info}
                             </blockquote>
                             <div className="author-info mt-4">
-                              <h6 className="mb-0">{testimonial.name} </h6>
-                              <span className="small">
-                                {testimonial.title}{" "}
-                              </span>
+                              <h6>{testimonial.name}</h6>
+                              <span className="small">{testimonial.title}</span>
                             </div>
                           </div>
                         </div>
                       ))}
                     </div>
+                    {/* Testimonials Navigation */}
                     <ul
                       className="nav testimonial-tab-list mt-5"
                       id="nav-tab"
                       role="tablist"
                     >
                       {registerTestimonialTarget.map((testimonialTarget, i) => (
-                        <li key={i + 1} className="nav-item">
+                        <li key={i} className="nav-item">
                           <a
                             className={testimonialTarget.active}
                             href={testimonialTarget.target}
@@ -164,64 +129,40 @@ const Register = () => {
                     </ul>
                   </div>
                 </div>
+                {/* Right Content */}
                 <div className="price-feature-col pricing-action-info p-5 right-radius bg-light order-0 order-lg-1">
-                  <a
-                    href="index.html"
-                    className="mb-5 d-block d-xl-none d-lg-none"
-                  >
-                    <img
-                      src="/logo-color.png"
-                      alt="logo"
-                      className="img-fluid"
-                    />
-                  </a>
                   <h1 className="h3">Create an Account</h1>
                   <p className="text-muted">
                     Get started with your free account today.
                   </p>
-
                   <form
                     onSubmit={handleSubmit(onSubmit)}
                     className="mt-5 register-form"
                   >
+                    {/* Form Inputs */}
                     <div className="row">
                       <div className="col-sm-6">
                         <label htmlFor="name" className="mb-1">
                           Name <span className="text-danger">*</span>
                         </label>
-                        <div className="input-group mb-3">
-                          <input
-                            {...register("name", { required: true })}
-                            type="text"
-                            className="form-control"
-                            placeholder="Name"
-                            id="name"
-                            required
-                            aria-label="name"
-                          />
-                          {errors.name && (
-                            <small className="text-red-600 text-sm ">
-                              This field is required
-                            </small>
-                          )}
-                        </div>
+                        <input
+                          {...register("name", { required: true })}
+                          type="text"
+                          className="form-control"
+                          placeholder="Name"
+                          id="name"
+                          required
+                          aria-label="name"
+                        />
+                        {errors.name && (
+                          <small className="text-red-600 text-sm">
+                            This field is required
+                          </small>
+                        )}
                       </div>
-                      <div className="col-sm-6 ">
-                        <label htmlFor="email" className="mb-1">
-                          Email <span className="text-danger">*</span>
-                        </label>
-                        <div className="input-group mb-3">
-                          <input
-                            {...register("email", { required: true })}
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                            id="email"
-                            required
-                            aria-label="email"
-                          />
-                        </div>
-                      </div>
+                      {/* Other Form Fields */}
+                      {/* Submit Button */}
+
                       <div className="col-sm-12">
                         <label htmlFor="company" className="mb-1">
                           Phone
@@ -284,6 +225,7 @@ const Register = () => {
                         Sign Up
                       </button>
                     </div>
+                    {/* Link to Login */}
                     <p className="text-center text-muted mt-4 mb-0 fw-medium font-monospace">
                       Already have an account?{" "}
                       <Link href="/login">
@@ -296,16 +238,16 @@ const Register = () => {
             </div>
           </div>
         </div>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar
-          closeOnClick
-          pauseOnHover
-          draggable
-          pauseOnFocusLoss
-        />
       </section>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        closeOnClick
+        pauseOnHover
+        draggable
+        pauseOnFocusLoss
+      />
     </Layout>
   );
 };
